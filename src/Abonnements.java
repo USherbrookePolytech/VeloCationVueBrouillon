562b8c2b9d9cd6828a8f677c08d7423bd7a45b21
Pierrick Bobet - Bebo <pierrick.bobet@cryptolab.net>
2017-11-12 09:26:18 -0500

    Correctif abonnement

--- src/Abonnements.java
+++ src/Abonnements.java
@@ -65,34 +65,40 @@ public class Abonnements
         frmAbonnement = new JFrame();
         frmAbonnement.setResizable(false);
         frmAbonnement.setTitle("Abonnement");
-        frmAbonnement.setBounds(100, 100, 555, 410);
+        frmAbonnement.setBounds(100, 100, 555, 429);
         frmAbonnement.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JPanel panelAbonnements = new JPanel();
         panelAbonnements.setBorder(new TitledBorder(null, "Choisissez votre abonnement", TitledBorder.LEADING, TitledBorder.TOP, null, null));
         
         JButton btnAnnulerLinscription = new JButton("Annuler l'inscription");
+        
+        JButton btnRetour = new JButton("Retour");
         GroupLayout groupLayout = new GroupLayout(frmAbonnement.getContentPane());
         groupLayout.setHorizontalGroup(
             groupLayout.createParallelGroup(Alignment.LEADING)
                 .addGroup(groupLayout.createSequentialGroup()
+                    .addContainerGap()
                     .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
                         .addGroup(groupLayout.createSequentialGroup()
-                            .addContainerGap()
-                            .addComponent(panelAbonnements, GroupLayout.DEFAULT_SIZE, 529, Short.MAX_VALUE))
-                        .addGroup(groupLayout.createSequentialGroup()
-                            .addGap(174)
-                            .addComponent(btnAnnulerLinscription, GroupLayout.PREFERRED_SIZE, 200, GroupLayout.PREFERRED_SIZE)))
-                    .addContainerGap())
+                            .addComponent(panelAbonnements, GroupLayout.DEFAULT_SIZE, 529, Short.MAX_VALUE)
+                            .addContainerGap())
+                        .addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+                            .addComponent(btnRetour)
+                            .addGap(84)
+                            .addComponent(btnAnnulerLinscription)
+                            .addGap(124))))
         );
         groupLayout.setVerticalGroup(
             groupLayout.createParallelGroup(Alignment.LEADING)
-                .addGroup(groupLayout.createSequentialGroup()
+                .addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
                     .addContainerGap()
-                    .addComponent(panelAbonnements, GroupLayout.PREFERRED_SIZE, 327, GroupLayout.PREFERRED_SIZE)
-                    .addGap(9)
-                    .addComponent(btnAnnulerLinscription)
-                    .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(panelAbonnements, GroupLayout.DEFAULT_SIZE, 349, Short.MAX_VALUE)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+                        .addComponent(btnRetour)
+                        .addComponent(btnAnnulerLinscription))
+                    .addContainerGap())
         );
         
         JPanel panelAbo1 = new JPanel();
@@ -110,28 +116,26 @@ public class Abonnements
         GroupLayout gl_panelAbo3 = new GroupLayout(panelAbo3);
         gl_panelAbo3.setHorizontalGroup(
             gl_panelAbo3.createParallelGroup(Alignment.TRAILING)
-                .addGap(0, 493, Short.MAX_VALUE)
                 .addGroup(gl_panelAbo3.createSequentialGroup()
                     .addContainerGap()
                     .addGroup(gl_panelAbo3.createParallelGroup(Alignment.LEADING)
-                        .addComponent(lbl3Mois)
-                        .addGroup(Alignment.TRAILING, gl_panelAbo3.createSequentialGroup()
+                        .addGroup(gl_panelAbo3.createSequentialGroup()
                             .addComponent(lblDetail3Mois)
                             .addPreferredGap(ComponentPlacement.RELATED, 93, Short.MAX_VALUE)
-                            .addComponent(btn3Mois)))
+                            .addComponent(btn3Mois))
+                        .addComponent(lbl3Mois))
                     .addContainerGap())
         );
         gl_panelAbo3.setVerticalGroup(
             gl_panelAbo3.createParallelGroup(Alignment.LEADING)
-                .addGap(0, 85, Short.MAX_VALUE)
                 .addGroup(gl_panelAbo3.createSequentialGroup()
                     .addContainerGap()
                     .addComponent(lbl3Mois)
-                    .addPreferredGap(ComponentPlacement.UNRELATED)
+                    .addGap(11)
                     .addGroup(gl_panelAbo3.createParallelGroup(Alignment.BASELINE)
                         .addComponent(btn3Mois, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(lblDetail3Mois))
-                    .addGap(10))
+                    .addContainerGap())
         );
         panelAbo3.setLayout(gl_panelAbo3);
         
@@ -153,14 +157,12 @@ public class Abonnements
                         .addComponent(lbl1An)
                         .addGroup(gl_panel1An.createSequentialGroup()
                             .addComponent(lblDetail1An)
-                            .addPreferredGap(ComponentPlacement.RELATED, 110, Short.MAX_VALUE)
-                            .addComponent(btn1An, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)))
+                            .addPreferredGap(ComponentPlacement.RELATED, 99, Short.MAX_VALUE)
+                            .addComponent(btn1An, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)))
                     .addContainerGap())
         );
         gl_panel1An.setVerticalGroup(
             gl_panel1An.createParallelGroup(Alignment.LEADING)
-                .addGap(0, 85, Short.MAX_VALUE)
-                .addGap(0, 85, Short.MAX_VALUE)
                 .addGroup(gl_panel1An.createSequentialGroup()
                     .addContainerGap()
                     .addComponent(lbl1An)
@@ -168,30 +170,34 @@ public class Abonnements
                     .addGroup(gl_panel1An.createParallelGroup(Alignment.BASELINE)
                         .addComponent(lblDetail1An)
                         .addComponent(btn1An, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGap(10))
+                    .addContainerGap())
         );
         panel1An.setLayout(gl_panel1An);
+        
+        JButton btnAide = new JButton("Aide");
         GroupLayout gl_panelAbonnements = new GroupLayout(panelAbonnements);
         gl_panelAbonnements.setHorizontalGroup(
             gl_panelAbonnements.createParallelGroup(Alignment.LEADING)
                 .addGroup(gl_panelAbonnements.createSequentialGroup()
                     .addContainerGap()
                     .addGroup(gl_panelAbonnements.createParallelGroup(Alignment.LEADING)
-                        .addComponent(panelAbo1, GroupLayout.DEFAULT_SIZE, 487, Short.MAX_VALUE)
-                        .addComponent(panelAbo3, GroupLayout.DEFAULT_SIZE, 497, Short.MAX_VALUE)
-                        .addComponent(panel1An, GroupLayout.DEFAULT_SIZE, 497, Short.MAX_VALUE))
+                        .addComponent(panel1An, GroupLayout.DEFAULT_SIZE, 497, Short.MAX_VALUE)
+                        .addComponent(panelAbo3, GroupLayout.DEFAULT_SIZE, 496, Short.MAX_VALUE)
+                        .addComponent(panelAbo1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnAide, Alignment.TRAILING))
                     .addContainerGap())
         );
         gl_panelAbonnements.setVerticalGroup(
             gl_panelAbonnements.createParallelGroup(Alignment.LEADING)
-                .addGroup(gl_panelAbonnements.createSequentialGroup()
-                    .addContainerGap()
+                .addGroup(Alignment.TRAILING, gl_panelAbonnements.createSequentialGroup()
+                    .addComponent(btnAide)
+                    .addPreferredGap(ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
                     .addComponent(panelAbo1, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(ComponentPlacement.UNRELATED)
+                    .addPreferredGap(ComponentPlacement.RELATED)
                     .addComponent(panelAbo3, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-                    .addPreferredGap(ComponentPlacement.UNRELATED)
+                    .addPreferredGap(ComponentPlacement.RELATED)
                     .addComponent(panel1An, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addContainerGap())
         );
         
         JLabel lbl1Mois = new JLabel("1 Mois -> $50 (CAN)");
@@ -212,7 +218,7 @@ public class Abonnements
                     .addGroup(gl_panelAbo1.createParallelGroup(Alignment.LEADING)
                         .addGroup(gl_panelAbo1.createSequentialGroup()
                             .addComponent(lblDetail1Mois)
-                            .addPreferredGap(ComponentPlacement.RELATED, 93, Short.MAX_VALUE)
+                            .addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(btn1Mois))
                         .addComponent(lbl1Mois))
                     .addContainerGap())
@@ -226,7 +232,7 @@ public class Abonnements
                     .addGroup(gl_panelAbo1.createParallelGroup(Alignment.BASELINE)
                         .addComponent(lblDetail1Mois)
                         .addComponent(btn1Mois, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGap(10))
+                    .addContainerGap())
         );
         panelAbo1.setLayout(gl_panelAbo1);
         panelAbonnements.setLayout(gl_panelAbonnements);
