a4fa5ead27e57521ebe47dcba6c4f230f466c155
Pierrick Bobet - Bebo <pierrick.bobet@cryptolab.net>
2017-11-12 17:45:46 -0500

    Fix

--- src/ConnexionTempValidation.java
+++ src/ConnexionTempValidation.java
@@ -6,7 +6,6 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
-import java.awt.Color;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
